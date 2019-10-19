3377493e46d99fa4a53805735ff55913e520231d
李欣悦 <1351057502@qq.com>
2019-10-19 17:44:41 +0800

    33

--- huxiaoyou/src/components/shopDetail/shopDetail.vue
+++ huxiaoyou/src/components/shopDetail/shopDetail.vue
@@ -137,7 +137,7 @@ export default {
                 var data=res.data.data
                 this.test=data.test
                     wx.config({
-                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        debug: ture, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                         appId: data.appId, // 必填，公众号的唯一标识
                         timestamp:data.timestamp, // 必填，生成签名的时间戳
                         nonceStr: data.nonceStr, // 必填，生成签名的随机串
@@ -164,7 +164,7 @@ export default {
          //   分享给朋友
       toFriend(){
                 var vm=this
-                var realLocation=vm.apiH+'/#/shopDetail?user_id='+vm.BuserIdH+'&goods_id='+vm.BshopId+'&goods_id='+
+                var realLocation=vm.apiH+'/#/shopDetail?user_id='+vm.BuserIdH+'&goods_id='+vm.BshopId
                 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
                     wx.onMenuShareAppMessage({ 
                         title:vm.shopData.goods_name, // 分享标题
