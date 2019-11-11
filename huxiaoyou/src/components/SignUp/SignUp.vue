8d04526d52003c21f4e9365342e84dc5b29cb6d5
李欣悦 <1351057502@qq.com>
2019-11-11 19:22:55 +0800

    提交代码

--- huxiaoyou/src/components/SignUp/SignUp.vue
+++ huxiaoyou/src/components/SignUp/SignUp.vue
@@ -86,8 +86,8 @@
                 </div>
             </div>
         </transition>
-      <!-- 恭喜您报名成功  v-if="showSignSuccess"-->
-      <div class="sign_success_wrap"  >
+      <!-- 恭喜您报名成功  -->
+      <div class="sign_success_wrap"  v-if="showSignSuccess">
          <div class="sign_success" >
               <div class="sign_Cha"><img @click.stop="signSuccessSha"  :src="staticImgH+'cha.png'" alt=""></div>
               <div class="sign_succeddImg"><span>恭喜您报名成功</span></div> 
