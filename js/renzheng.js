8bb62b525e74a65fdda7626407f90865e0c955e4
李欣悦 <1351057502@qq.com>
2019-11-22 21:08:49 +0800

    提交

---
+++ js/renzheng.js
@@ -0,0 +1,28 @@
+// 获取域名
+      var domainName=''//初始化域名
+      if(window.location.protocol+"//"+window.location.host=="http://ceshi.aibebi.net"){
+          domainName=window.location.protocol+"//"+window.location.host
+      }else{
+          domainName='http://www.aibebi.net'
+      }
+
+$(".Certi_wrapper").on("tap",'.renSumbit',function(){
+		var userName=$("input[name='userName']").val()
+		var IDCard=$("input[name='IDCard']").val()
+		var PhoneNum=$("input[name='PhoneNum']").val()
+		var BankCard=$("input[name='BankCard']").val()
+	 	var personInfo= $.param({'mobile':PhoneNum,'real_name':userName,'id_card':IDCard,'bank_card':BankCard,'type':3})
+              $.ajax({
+                url:domainName+'/user/real_Name',
+               	type:'post',
+                async:false,
+                datatype:'jsonp',
+                data: personInfo,
+                jsonp:"callback",
+                success:function(res){
+                  console.log(res)
+                 }
+
+              })
+		// window.location.href="renzheng.html"
+	})
