b3bec671cd6c2f3908d076d166c1dec42a85df7d
李欣悦 <1351057502@qq.com>
2019-11-22 14:32:47 +0800

    提交代码

--- huxiaoyou/src/components/PresentVideo/PresentVideo.vue
+++ huxiaoyou/src/components/PresentVideo/PresentVideo.vue
@@ -33,10 +33,10 @@
       </div>
     </transition>
     <!-- loding -->
-      <div class="login_wrap" v-if="lodingShow">
-         <div class="loginImg">
+      <div class="login_wrap" v-if="lodingShow" >
+         <div class="loginImg" >
            <img :src="staticImgH+'jiazai.gif'" alt="">
-           正在上传，请稍等
+           正在上传，请稍等...
          </div>
       </div>
   </div>
@@ -315,20 +315,19 @@
   left:0;
   z-index:999;
   .loginImg{
-    width:1.28rem;
-    height:1.28rem;
-    position:absolute;
-    top:0;
-    bottom:0;
-    left:0;
-    right:0;
+     width:100%;
+    height:100%;
     margin:auto;
     display:flex;
     flex-direction:column;
     align-items :center;
+    justify-content :center;
+    font-size:0.347rem;
+    color:#fff;
     >img{
-      width:100%;
-      height:100%;
+      width:1.28rem;
+      height:1.28rem;
+      margin-bottom:0.32rem;
     }
   }
 }
